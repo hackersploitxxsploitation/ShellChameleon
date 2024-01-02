a6b428b462215d53268b2387d4c9c2dcd8d248c0
Hackersploit__xxSploitation <78088921+hackersploitxxsploitation@users.noreply.github.com>
2024-01-01 21:34:31 -0300

    Update shellcode.go

--- shellcode.go
+++ shellcode.go
@@ -1,57 +1,70 @@
-	//Gerador de Shellcoder Polimorfico
-	package main
-	import (
-		"fmt"
-		"math/rand"
-		"time"
-	)
-	var shell []string
-	func Crossover(shellcode_parent1 string,shellcode_parent2 string)(string){
-	rand.Seed(time.Now().UnixNano())
-	fuction_point_croosver:=rand.Intn(len(shellcode_parent1))
-	Child:=shellcode_parent1[:fuction_point_croosver]+shellcode_parent2[:fuction_point_croosver]
-	return Child
-	
-	
-	
-	
-	}
-	
-	func Mutation( xild string)(string){
-	rand.Seed(time.Now().UnixNano())
-	Ponto_de_Mutaçao:= rand.Intn(len(xild))
-	child_mutaton:= xild[:Ponto_de_Mutaçao] + string(byte((int(xild[Ponto_de_Mutaçao])+1)%256)) + xild[Ponto_de_Mutaçao+1:]
-	return child_mutaton
-	
-	}
-	
-	
-	func main(){
-	var    shellcode string
-	//var  number int
-	
-	fmt.Println("######::'##::::'##:'########:'##:::::::'##:::::: ::'######::'##::::'##::::'###:::'##::::'##:'##### ###:'##:::::::'########::'#######::'##::: ##:")
-	fmt.Println("'##... ##: ##:::: ##: ##.....:: ##::::::: ##:::::::'##.. . ##: ##:::: ##:::'## ##::: ###::'###: ##.....:: ##:::::: : ##.....::'##.... ##: ###:: ##:")
-	fmt.Println("##:::..:: ##:::: ##: ##::::::: ##::::::: ##::::::: ##::: ..:: ##:::: ##::'##:. ##:: ####'####: ##::::::: ##::::::: ##::::::: ##:::: ##: ####: ##:")
-	fmt.Println(". ######:: #########: ######::: ##::::::: ##::::::: ##::: :::: #########:'##:::. ##: ## ### ##: ######::: ##::::::: ######::: ##:::: ##: ## # ###:")
-	fmt.Println(":..... ##: ##.... ##: ##...:::: ##::::::: ##::::::: ##::: :::: ##.... ##: #########: ##. #: ##: ##...:::: ##::::::: ##...:::: ##:::: ##: ##. ####:")
-	fmt.Println("'##::: ##: ##:::: ##: ##::::::: ##::::::: ##::::::: ##::: ##: ##:::: ##: ##.... ##: ##:.:: ##: ##::::::: ##::::::: ## ::::::: ##:::: ##: ##:. ###:")
-	fmt.Println(". ######:: ##:::: ##: ########: ########: ########:. ######:: ##:::: ##: ##:::: ##: ##:::: ##: ########: ###### ##: ########:. #######:: ##::. ##:")
-	fmt.Println(":......:::..:::::..::........::........::........: ::......:::..:::::..::..:::::..::..:::::..::...... ..::........::........:::.....:::..::::..::")
-	
-	fmt.Println("Digite  o Shellcode :")
-	fmt.Scanf("%s",&shellcode)
-	//fmt.Println("Digte o Numero de Variantes ")
-	//fmt.Scanf("%d",&number)
-		var variants []string
-	for i := 0; i < 33; i++ {
-		parent1 := shellcode
-		parent2 := shellcode
-		child := Crossover(parent1, parent2)
-		mutatedChild := Mutation(child)
-		variants = append(variants, mutatedChild)
-	}
-	for index, value := range variants {
-        fmt.Printf("Indexado: %d, Valor: %s\n", index, value)
-    }
-	}
+		//Gerador de Shellcoder Polimorfico
+		package main
+		import (
+			"fmt"
+			"math/rand"
+			"time"
+		)
+		var shell []string
+		func Crossover(shellcode_parent1 string,shellcode_parent2 string)(string){
+		rand.Seed(time.Now().UnixNano())
+		fuction_point_croosver:=rand.Intn(len(shellcode_parent1))
+		Child:=shellcode_parent1[:fuction_point_croosver]+shellcode_parent2[:fuction_point_croosver]
+		return Child
+		
+		
+		
+		
+		}
+		
+		func Mutation( xild string)(string){
+		rand.Seed(time.Now().UnixNano())
+		Ponto_de_Mutaçao:= rand.Intn(len(xild))
+		child_mutaton:= xild[:Ponto_de_Mutaçao] + string(byte((int(xild[Ponto_de_Mutaçao])+1)%256)) + xild[Ponto_de_Mutaçao+1:]
+		return child_mutaton
+		
+		}
+		
+		func Genrate_payload(shellcode string,number int){
+		var mop []string
+		for i := 0; i < number; i++ {
+			shellcode_parent1 := shellcode
+			shellcode_parent2 := shellcode
+			Child := Crossover(shellcode_parent1, shellcode_parent2)
+			MutatedChild := Mutation(Child)
+			mop = append(mop, MutatedChild)
+		}
+		for iter, shell_code := range mop {
+			fmt.Printf("indexado: %d, valor: %s\n", iter, shell_code)
+		}
+		
+		
+		}
+		func main(){
+		
+		
+		fmt.Println("######::'##::::'##:'########:'##:::::::'##:::::: ::'######::'##::::'##::::'###:::'##::::'##:'##### ###:'##:::::::'########::'#######::'##::: ##:")
+		fmt.Println("'##... ##: ##:::: ##: ##.....:: ##::::::: ##:::::::'##.. . ##: ##:::: ##:::'## ##::: ###::'###: ##.....:: ##:::::: : ##.....::'##.... ##: ###:: ##:")
+		fmt.Println("##:::..:: ##:::: ##: ##::::::: ##::::::: ##::::::: ##::: ..:: ##:::: ##::'##:. ##:: ####'####: ##::::::: ##::::::: ##::::::: ##:::: ##: ####: ##:")
+		fmt.Println(". ######:: #########: ######::: ##::::::: ##::::::: ##::: :::: #########:'##:::. ##: ## ### ##: ######::: ##::::::: ######::: ##:::: ##: ## # ###:")
+		fmt.Println(":..... ##: ##.... ##: ##...:::: ##::::::: ##::::::: ##::: :::: ##.... ##: #########: ##. #: ##: ##...:::: ##::::::: ##...:::: ##:::: ##: ##. ####:")
+		fmt.Println("'##::: ##: ##:::: ##: ##::::::: ##::::::: ##::::::: ##::: ##: ##:::: ##: ##.... ##: ##:.:: ##: ##::::::: ##::::::: ## ::::::: ##:::: ##: ##:. ###:")
+		fmt.Println(". ######:: ##:::: ##: ########: ########: ########:. ######:: ##:::: ##: ##:::: ##: ##:::: ##: ########: ###### ##: ########:. #######:: ##::. ##:")
+		fmt.Println(":......:::..:::::..::........::........::........: ::......:::..:::::..::..:::::..::..:::::..::...... ..::........::........:::.....:::..::::..::")
+		var shellcode string
+    var variantes int
+
+    fmt.Println("Digite o shellcode:")
+    fmt.Scanln(&shellcode)
+
+    fmt.Println("Digite o  numero de variantes:")
+    fmt.Scanln(&variantes)
+	Genrate_payload(shellcode,variantes)
+
+    
+		
+		
+		
+			
+		
+		}
